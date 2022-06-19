d2dee39814361d0d2167523183475ebe84c2efff
Smolder98 <alicoescobar@gmail.com>
2022-06-19 13:53:19 -0600

    prueba

---
+++ PM2E10280.Android/Services/ILocSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM2E10280.Services
+{
+    public interface ILocSettings
+    {
+        void OpenSettings();
+
+        bool isGpsAvailable();
+
+    }
+}
